6c89280d94ad2bb24b49355c352ebbaa3f6624c2
jherbage <jasonherbage@googlemail.com>
2019-02-07 15:36:28 +0000

    correct cert path in config

--- kubernetesTest.py
+++ kubernetesTest.py
@@ -1,8 +1,15 @@
 from kubernetes import client, config
 
 def handler(event,context):
+  # Check the cert paths are relative to the working folder
+  f1 = open('config', 'r')
+  f2 = open('config-updated', 'w')
+  for line in f1:
+    f2.write(line.replace('/certs/', ''))
+  f1.close()
+  f2.close()
   # Configs can be set in Configuration class directly or using helper utility
-  config.load_kube_config('config')
+  config.load_kube_config('config-updated')
 
   v1 = client.CoreV1Api()
   print("Listing pods with their IPs:")
