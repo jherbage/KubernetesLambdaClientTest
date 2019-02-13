d3ab6cdf433ffb31427a0faf1e85979abcdc8259
jherbage <jasonherbage@googlemail.com>
2019-02-13 11:35:06 +0000

    adding yaml for the smoke test deployment update port

--- kubernetesTest.py
+++ kubernetesTest.py
@@ -138,7 +138,7 @@ def handler(event,context):
 	# Check we can contact port 30101 on both IPs
     for ip in private_ips:
       contents = urllib2.urlopen("http://"+ip+":30101").read()
-	  if "nginx" not in contents.lower():
+      if "nginx" not in contents.lower():
         print "Could not contact K8 node nginx container on "+ip+" port 30101"
         sys.exit(1)
       else:
